774fb43040e1e4b24d0101d5c813eb5cd00343d6
Pratik Kesharwani <104644580+pratikkw@users.noreply.github.com>
2023-11-06 08:36:19 -0500

    added suggestion box

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,11 @@ const darkModeBtn = document.getElementById("darkmode");
 darkModeBtn.addEventListener("click", function () {
   document.body.classList.toggle("darkmode");
 });
+
+document
+  .getElementById("location__icon")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".suggestion__box")
+      .classList.toggle("suggestion__box--active");
+  });
